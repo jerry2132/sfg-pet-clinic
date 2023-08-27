5f1576cce03ac48f52457383d35927ae1ac39cb0
jerry2132 <sumitsinghrawat34@gmail.com>
2023-08-27 15:52:45 +0530

    commit applied

---
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package sumit.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package sumit.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+}
